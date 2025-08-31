3157abcf2d82523da200697510ef604cffbfca04
SivaDinesh2094 <166581790+SivaDinesh2094@users.noreply.github.com>
2025-08-31 13:17:34 +0530

    Delete assignment3 Task2.py

--- assignment3 Task2.py
+++
@@ -1,9 +0,0 @@
-import math
-
-num = int(input("Enter a number: "))
-root = math.sqrt(num)
-log = math.log(num)
-sin = math.sin(num)
-print("Square root:",root)
-print("Logarithm:",log)
-print("Sine:",sin)
