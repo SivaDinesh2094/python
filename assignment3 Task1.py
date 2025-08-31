1cccb7ed841b2c86781edb9eedd55ad04e542b72
SivaDinesh2094 <166581790+SivaDinesh2094@users.noreply.github.com>
2025-08-31 13:16:19 +0530

    Update assignment3 Task1.py

--- assignment3 Task1.py
+++ assignment3 Task1.py
@@ -13,7 +13,17 @@ result = fact(value)
 
 print('Factorial of',value,'is :',result)
 
+#task 2
 
+import math
+
+num = int(input("Enter a number: "))
+root = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+print("Square root:",root)
+print("Logarithm:",log)
+print("Sine:",sin)
 
 
 
